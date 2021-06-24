b45debd96fb81c75d1115d50ff9abffe4dee292e
v-datsenko <datsenkovalerij37@gmail.com>
2021-06-24 20:50:21 +0300

    Added search functionality for all pages of the site.

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ApplicationLibrary;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestTask
 {
@@ -9,9 +11,27 @@ namespace TestTask
         {
             Console.WriteLine("Enter site url!");
             string url = Console.ReadLine();
-            var crawler = new Crawler(url);
-            crawler.StartCrawlerAsync();
-            
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+            var crawler = new Crawler(url, token);
+
+            Task scan = crawler.StartCrawlerAsync();
+            Thread consoleRead = new Thread(() =>
+            {
+                ConsoleKeyInfo key = new ConsoleKeyInfo();
+                while (key.KeyChar != 'c')
+                {
+                    key = Console.ReadKey();
+                }
+                cancelTokenSource.Cancel();
+            });
+            consoleRead.Start();
+            scan.Wait();
+            foreach(var link in crawler.FinalUriList)
+            {
+                Console.WriteLine(link);
+            }
+            Console.WriteLine("Press any key to exit!");
             Console.ReadKey();
         }
     }
