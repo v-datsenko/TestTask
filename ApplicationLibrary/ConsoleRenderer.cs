0538d777df49d79fe1469e8faa64d5b10214cff5
v-datsenko <datsenkovalerij37@gmail.com>
2021-06-25 17:12:12 +0300

    Implemented class ConsoleRenderer. Added the output of list with urls that exists in sitemap and doesn't on website pages. Added the output list with urls (html documents) which exists on web site but doesn’t in sitemap.xml.

---
+++ ApplicationLibrary/ConsoleRenderer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationLibrary
+{
+    public static class ConsoleRenderer
+    {
+        public static int TableWidth { get; set; } = 73;
+        public static Func<string, int, string> AlignFunc = AlignCentre;
+        public static void DisplayList(string header, IEnumerable<string> list)
+        {
+            Console.WriteLine(header);
+            int i = 1;
+            foreach (var str in list)
+            {
+                Console.WriteLine($"{i}) {str}");
+                i++;
+            }
+        }
+
+        public static void DisplayListInTable(string title,List<string> headers, params List<string>[] listOfList)
+        {
+            Console.WriteLine(title);
+            PrintLine();
+            PrintRow(0, headers.ToArray());
+            PrintLine();
+
+            var array = new string[listOfList[0].Count][];
+            for (int i = 0; i < listOfList[0].Count; i++)
+            {
+                array[i] = new string[headers.Count()];
+            }
+            for (int i = 0; i < listOfList[0].Count; i++)
+            {
+                for (int j = 0; j < headers.Count(); j++)
+                {
+                    array[i][j] = listOfList[j][i];
+                }
+            }
+
+            for (int i = 0; i < listOfList[0].Count; i++)
+            {
+                PrintRow(i + 1, array[i].ToArray());
+            }
+            PrintLine();
+        }
+
+        public static void PrintLine()
+        {
+            Console.WriteLine(new string('-', TableWidth));
+        }
+
+        public static void PrintRow(int numberLine, params string[] columns)
+        {
+            int width = (TableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+
+                row += numberLine == 0 ? AlignFunc(column, width) + "|" : AlignFunc($"{numberLine}) {column}", width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        public static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        public static string AlignLeft(string text, int width)
+        {
+            text = " " + text;
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width);
+            }
+        }
+    }
+}
--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -9,7 +9,7 @@ namespace TestTask
 {
     class Program
     {
-        static int tableWidth = 73;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Enter site url!");
@@ -32,96 +32,40 @@ namespace TestTask
             scan.Wait();
 
             List<string> foundLinks = crawler.FinalUriList.Select(u => u.ToString()).ToList();
-            DisplayList("\nFound available links: ", foundLinks);
+            ConsoleRenderer.DisplayList("\nFound available links: ", foundLinks);
 
             var sitemapProvider = new SitemapProvider(crawler.Uri);
             List<string> sitemapList = sitemapProvider.GetSitemapFromSite();
 
+            bool sitemap = false;
+            List<string> allLinks;
             if (sitemapList.Count != 0)
             {
-                DisplayList("\nAll links from sitemap.xml:", sitemapList);
+                ConsoleRenderer.DisplayList("\nAll links from sitemap.xml:", sitemapList);
+                sitemap = true;
+                allLinks = foundLinks.Union(sitemapList).Distinct().ToList();
             }
             else
             {
-                Console.WriteLine("Sitemap could not be retrieved!");
+                allLinks = foundLinks;
             }
 
-            List<string> allLinks = foundLinks.Union(sitemapList).Distinct().ToList();
 
-            DisplayListInTable(new List<string> { "Url"}, new List<string>[] { allLinks });
+            if (sitemap)
+            {
+                List<string> displayedList = sitemapList.Except(foundLinks).ToList();
+                ConsoleRenderer.TableWidth = 90;
+                ConsoleRenderer.AlignFunc = ConsoleRenderer.AlignLeft;
+                ConsoleRenderer.DisplayListInTable("\nUrls FOUNDED IN SITEMAP.XML but not founded after crawling a web site", new List<string> { "Url" }, new List<string>[] { displayedList });
+
+                displayedList = foundLinks.Except(sitemapList).ToList();
+                ConsoleRenderer.DisplayListInTable("\nUrls FOUNDED BY CRAWLING THE WEBSITE but not in sitemap.xml", new List<string> { "Url" }, new List<string>[] { displayedList });
+            }
 
             Console.WriteLine("Press any key to exit!");
             Console.ReadKey();
         }
 
-        public static void DisplayList(string header,IEnumerable<string> list)
-        {
-            Console.WriteLine(header);
-            int i = 1;
-            foreach (var str in list)
-            {
-                Console.WriteLine(i + str);
-                i++;
-            }
-        }
-
-        public static void DisplayListInTable(List<string> headers, params List<string>[] listOfList)
-        {
-            PrintLine();
-            PrintRow(0,headers.ToArray());
-            PrintLine();
-
-            var array = new string[listOfList[0].Count][];
-            for (int i = 0; i < listOfList[0].Count; i++)
-            {
-                array[i] = new string[headers.Count()];
-            }
-            for (int i = 0; i < listOfList[0].Count; i++)
-            {
-                for (int j = 0; j < headers.Count(); j++)
-                {
-                    array[i][j] = listOfList[j][i];
-                }
-            }
-
-            for (int i = 0; i < listOfList[0].Count; i++)
-            {
-                PrintRow(i + 1,array[i].ToArray());
-            }
-            PrintLine();
-        }
-
-        static void PrintLine()
-        {
-            Console.WriteLine(new string('-', tableWidth));
-        }
-
-        static void PrintRow(int numberLine,params string[] columns)
-        {
-            int width = (tableWidth - columns.Length) / columns.Length;
-            string row = "|";
-
-            foreach (string column in columns)
-            {
-                
-                row += numberLine == 0 ? AlignCentre(column, width) + "|" : AlignCentre($"{numberLine}) {column}", width) + "|";
-            }
-
-            Console.WriteLine(row);
-        }
-
-        static string AlignCentre(string text, int width)
-        {
-            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
-
-            if (string.IsNullOrEmpty(text))
-            {
-                return new string(' ', width);
-            }
-            else
-            {
-                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
-            }
-        }
+        
     }
 }
