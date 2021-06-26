d4b41c9811065f9b5f75cbf7027f60d038e70d93
v-datsenko <datsenkovalerij37@gmail.com>
2021-06-26 18:23:58 +0300

    Added the try-catch statement in the Program class.

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -9,63 +9,67 @@ namespace TestTask
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter site url!");
-            string url = Console.ReadLine();
-            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
-            CancellationToken token = cancelTokenSource.Token;
-            var crawler = new Crawler(url, token);
-
-            Task scan = crawler.StartCrawlerAsync();
-            Thread consoleRead = new Thread(() =>
+            try
             {
-                ConsoleKeyInfo key = new ConsoleKeyInfo();
-                while (key.KeyChar != 'c')
+                Console.WriteLine("Enter site url!");
+                string url = Console.ReadLine();
+                CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+                CancellationToken token = cancelTokenSource.Token;
+                var crawler = new Crawler(url, token);
+
+                Task scan = crawler.StartCrawlerAsync();
+                Thread consoleRead = new Thread(() =>
                 {
-                    key = Console.ReadKey();
+                    ConsoleKeyInfo key = new ConsoleKeyInfo();
+                    while (key.KeyChar != 'c')
+                    {
+                        key = Console.ReadKey();
+                    }
+                    cancelTokenSource.Cancel();
+                });
+                consoleRead.Start();
+                scan.Wait();
+
+                List<string> foundLinks = crawler.FinalUriList.Select(u => u.ToString()).ToList();
+                ConsoleRenderer.DisplayList("\nFound available links: ", foundLinks);
+
+                var sitemapProvider = new SitemapProvider(crawler.Uri);
+                List<string> sitemapList = sitemapProvider.GetSitemapFromSite();
+
+                bool sitemap = false;
+                List<string> allLinks;
+                if (sitemapList.Count != 0)
+                {
+                    ConsoleRenderer.DisplayList("\nAll links from sitemap.xml:", sitemapList);
+                    sitemap = true;
+                    allLinks = foundLinks.Union(sitemapList).Distinct().ToList();
                 }
-                cancelTokenSource.Cancel();
-            });
-            consoleRead.Start();
-            scan.Wait();
+                else
+                {
+                    allLinks = foundLinks;
+                }
 
-            List<string> foundLinks = crawler.FinalUriList.Select(u => u.ToString()).ToList();
-            ConsoleRenderer.DisplayList("\nFound available links: ", foundLinks);
 
-            var sitemapProvider = new SitemapProvider(crawler.Uri);
-            List<string> sitemapList = sitemapProvider.GetSitemapFromSite();
+                if (sitemap)
+                {
+                    List<string> displayedList = sitemapList.Except(foundLinks, new UrlComparer()).ToList();
+                    ConsoleRenderer.TableWidth = 90;
+                    ConsoleRenderer.AlignFunc = ConsoleRenderer.AlignLeft;
+                    ConsoleRenderer.DisplayListInTable("\nUrls FOUNDED IN SITEMAP.XML but not founded after crawling a web site", new List<string> { "Url" }, new List<string>[] { displayedList });
 
-            bool sitemap = false;
-            List<string> allLinks;
-            if (sitemapList.Count != 0)
-            {
-                ConsoleRenderer.DisplayList("\nAll links from sitemap.xml:", sitemapList);
-                sitemap = true;
-                allLinks = foundLinks.Union(sitemapList).Distinct().ToList();
+                    displayedList = foundLinks.Except(sitemapList, new UrlComparer()).ToList();
+                    ConsoleRenderer.DisplayListInTable("\nUrls FOUNDED BY CRAWLING THE WEBSITE but not in sitemap.xml", new List<string> { "Url" }, new List<string>[] { displayedList });
+                }
+
+                Console.WriteLine("Press any key to exit!");
+                Console.ReadKey();
             }
-            else
+            catch(Exception ex)
             {
-                allLinks = foundLinks;
+                Console.WriteLine(ex.Message);
             }
-
-
-            if (sitemap)
-            {
-                List<string> displayedList = sitemapList.Except(foundLinks, new UrlComparer()).ToList();
-                ConsoleRenderer.TableWidth = 90;
-                ConsoleRenderer.AlignFunc = ConsoleRenderer.AlignLeft;
-                ConsoleRenderer.DisplayListInTable("\nUrls FOUNDED IN SITEMAP.XML but not founded after crawling a web site", new List<string> { "Url" }, new List<string>[] { displayedList });
-
-                displayedList = foundLinks.Except(sitemapList, new UrlComparer()).ToList();
-                ConsoleRenderer.DisplayListInTable("\nUrls FOUNDED BY CRAWLING THE WEBSITE but not in sitemap.xml", new List<string> { "Url" }, new List<string>[] { displayedList });
-            }
-
-            Console.WriteLine("Press any key to exit!");
-            Console.ReadKey();
         }
-
-        
     }
 }
